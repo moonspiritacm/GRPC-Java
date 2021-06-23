c1106fd22fdac1f01fec3d9f5f15b6762472343b
moonspiritacm <yefanbupt@gmail.com>
2021-06-23 22:12:45 +0800

    v1

---
+++ src/main/java/com/moonspirit/turtorial/grpc/Application.java
@@ -0,0 +1,8 @@
+package com.moonspirit.turtorial.grpc;
+
+public class Application {
+    public static void main(String[] args) {
+        String ver = System.getProperty("java.version");
+        System.out.println(ver);
+    }
+}
